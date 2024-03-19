6122cb9b189acd40082197fd7b7c30b5aaebaf4c
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-03-19 15:50:59 +0000

    Starting Project

--- MusicByLocation/ContentView.swift
+++ MusicByLocation/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello World")
+                .padding()
+            Spacer()
+            Button("Find Music", action: {})
         }
-        .padding()
     }
 }
 
