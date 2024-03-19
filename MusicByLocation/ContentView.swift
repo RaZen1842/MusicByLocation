ce8b702ec8cd1fafaaff6412dce5f23fe6dd1dc5
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-03-19 15:43:06 +0000

    Initial Commit

---
+++ MusicByLocation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicByLocation
+//
+//  Created by Veturi, Shreyas (HJRM) on 19/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Veturi, Shreyas (HJRM) on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
